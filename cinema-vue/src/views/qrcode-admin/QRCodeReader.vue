0b50db3109d72b33a94bd6b6225524be798ce2b9
Filipe Copola <filipe@ficopola.net>
2023-07-12 16:07:17 +0200

    == Agiornamento VUE ==

---
+++ cinema-vue/src/views/qrcode-admin/QRCodeReader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <qrcode-stream @decode="handleDecode"></qrcode-stream>
+    <div v-if="qrCodeContent" class="result">{{ qrCodeContent }}</div>
+  </div>
+</template>
+
+<script>
+import { QrcodeStream } from "vue-qrcode-reader";
+
+export default {
+  components: {
+    QrcodeStream
+  },
+  data() {
+    return {
+      qrCodeContent: null
+    };
+  },
+  methods: {
+    handleDecode(content) {
+      this.qrCodeContent = content;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.result {
+  margin-top: 20px;
+  font-weight: bold;
+}
+</style>
